7b59cd5cb7b748a2f7504f4639db0ce208017695
drardin <dakota.rardin@gmail.com>
2023-10-21 16:47:28 -0600

    Added support for command line arguments, --debug being first one Moved debug output to new debug function Added support for new DIRECTORIES variable in config.env Renamed functions for consistency and clarity Removed line 81: echo "$LATEST_VER" >> "$SERVER_DOWNLOAD_LOG" as it was not needed

--- docker_data/bootstrap.sh
+++ docker_data/bootstrap.sh
@@ -9,23 +9,44 @@ else
     exit 1
 fi
 
-#### --- FUNCTIONS START --- ####
-initDirs() {
-    # List of directories to initialize
-    DIRECTORIES=("DEBUG" "main" "backups" "backups/downloads" "tmp" "logs" "logs")
+# Check for command line arguments
+if [[ "$#" -gt 0 && "$1" == "--debug" ]]; then
+    DEBUG=true
+else
+    DEBUG=false
+fi
 
-    # Loop through the directories and check if they exist, if not, create them
-    for dir in "${DIRECTORIES[@]}"; do
-        if [ ! -d "$dir" ]; then
-            mkdir "$dir"
-            echo "Created directory: $dir"
+#### --- FUNCTIONS START --- ####
+debug() {
+    if [ "$DEBUG" = true ]; then
+        echo "MAIN_PATH: $MAIN_PATH"
+        echo "DEBUG_PATH: $DEBUG_PATH"
+        echo "TEMP_PATH: $TEMP_PATH"
+        echo "LOG_PATH: $LOG_PATH"
+        echo "SERVER_DOWNLOAD_LOG: $SERVER_DOWNLOAD_LOG"
+        echo "LATEST_VER: $LATEST_VER"
+        echo "LATEST_URL: $LATEST_URL"
+        echo "BEDROCK_SERVER_PATH: $BEDROCK_SERVER_PATH"
+        echo "SERVER_NAME: $SERVER_NAME"
+        echo "MAX_PLAYERS: $MAX_PLAYERS"
+        echo "SERVER_PORT: $SERVER_PORT"
+    fi
+}
+
+initDirs() {
+    IFS=',' read -ra DIR_ARRAY <<< "$DIRECTORIES"
+    for dir in "${DIR_ARRAY[@]}"; do
+        dir_path="$APP_HOME/$dir"
+        if [ ! -d "$dir_path" ]; then
+            mkdir -p "$dir_path"
+            echo "Created directory: $dir_path"
         else
-            echo "Directory already exists: $dir"
+            echo "Directory already exists (skipping creation): $dir_path"
         fi
     done
 }
 
-getLatestVersion() {
+getLatestBDSVersion() {
     local download_page
     download_page=$(curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36" --no-progress-meter -s https://www.minecraft.net/en-us/download/server/bedrock)
 
@@ -42,7 +63,7 @@ getLatestVersion() {
 }
 
 
-extractFiles() {
+extractBDS() {
     local zip_file="$1"
 
     if [ ! -e "$FIRST_RUN_FLAG" ]; then
@@ -69,7 +90,7 @@ extractFiles() {
 }
 
 
-updateMinecraftServer() {
+updateBDS() {
     local download_filename="bedrock-server-$LATEST_VER.zip"
 
     # Check if SERVER_DOWNLOAD_LOG file exists
@@ -78,8 +99,7 @@ updateMinecraftServer() {
         # Download latest version of BDS
         echo "Downloading Minecraft Bedrock server version: $LATEST_VER from $LATEST_URL"
         curl -o "$DOWNLOADS_PATH/$download_filename" "$LATEST_URL"
-        echo "$LATEST_VER" >> "$SERVER_DOWNLOAD_LOG"
-        extractFiles "$download_filename"
+        extractBDS "$download_filename"
     else
         # Check if an update is needed
         local last_version
@@ -90,7 +110,7 @@ updateMinecraftServer() {
             echo "Downloading Minecraft Bedrock server version: $LATEST_VER from $LATEST_URL"
             curl -o "$DOWNLOADS_PATH/$download_filename" "$LATEST_URL"
             echo "$LATEST_VER" >> "$SERVER_DOWNLOAD_LOG"
-            extractFiles "$download_filename"
+            extractBDS "$download_filename"
         else
             echo "Server is already up to date. Skipping download."
         fi
@@ -98,10 +118,10 @@ updateMinecraftServer() {
 }
 
 
-initialize_server() {
+initBDS() {
 cd "$MAIN_PATH" || exit
-getLatestVersion
-updateMinecraftServer
+getLatestBDSVersion
+updateBDS
 }
 #### --- FUNCTIONS END --- ####
 
@@ -113,7 +133,7 @@ main() {
     initDirs
 
     # Perform server initialization
-    initialize_server
+    initBDS
 
     # Start server using Box64
     #box64 $BEDROCK_SERVER_PATH
@@ -124,18 +144,3 @@ main() {
 
 # Call the main function
 main
-
-#### --- DEBUGGING START --- ####
-echo "MAIN_PATH: $MAIN_PATH"
-echo "DEBUG_PATH: $DEBUG_PATH"
-echo "TEMP_PATH: $TEMP_PATH"
-echo "LOG_PATH: $LOG_PATH"
-echo "SERVER_DOWNLOAD_LOG: $SERVER_DOWNLOAD_LOG"
-echo "LATEST_VER: $LATEST_VER"
-echo "LATEST_URL: $LATEST_URL"
-echo "BEDROCK_SERVER_PATH: $BEDROCK_SERVER_PATH"
-echo "SERVER_NAME: $SERVER_NAME"
-echo "MAX_PLAYERS: $MAX_PLAYERS"
-echo "SERVER_PORT: $SERVER_PORT"
-cat 
-#### --- DEBUGGING END --- ####
