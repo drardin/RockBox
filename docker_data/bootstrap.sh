28e3e3999c2f57023d56b950e00194e0ae06ad18
drardin <dakota.rardin@gmail.com>
2023-10-21 04:13:58 -0600

    Several Updates to bootstrap.sh: Added logs file Added getLatestVersion function that will pull two important variables, LATEST_URL and LATEST_VER. These will be used in the updateMinecraftServer function. Added updateMinecraftServer function that downloads the current version of BDS if it hasn't already been downloaded and calls a function to perform the update. Added extractFiles function that sets a FIRST_RUN_FLAG, if not already set. It uses env var PROTECTED_FILES to prevent overwriting user-defined files and folders. Default value for this includes server.properties, allowlist.json and permissions.json. Added extra debugging info
    More work is needed for structuring code

--- docker_data/bootstrap.sh
+++ docker_data/bootstrap.sh
@@ -10,9 +10,9 @@ else
 fi
 
 #### --- FUNCTIONS START --- ####
-initialize_dirs() {
+initDirs() {
     # List of directories to initialize
-    DIRECTORIES=("DEBUG" "main" "backups" "tmp")
+    DIRECTORIES=("DEBUG" "main" "backups" "tmp" "logs")
 
     # Loop through the directories and check if they exist, if not, create them
     for dir in "${DIRECTORIES[@]}"; do
@@ -25,12 +25,68 @@ initialize_dirs() {
     done
 }
 
-#initialize_server() {
-    # Create server configuration files, set up properties, etc.
-    # Example:
-    # echo "server-name=myserver" > server.properties
-    # ...
-#}
+getLatestVersion() {
+    local download_page
+    download_page=$(curl -H "Accept-Encoding: identity" -H "Accept-Language: en" -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36" --no-progress-meter -s https://www.minecraft.net/en-us/download/server/bedrock)
+
+    local latest_url
+    latest_url=$(echo "$download_page" | grep -o 'https://minecraft.azureedge.net/bin-linux/bedrock-server-[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\.zip' | head -n 1)
+
+    if [ -n "$latest_url" ]; then
+        export LATEST_URL="$latest_url"
+        export LATEST_VER=$(basename "$latest_url" | sed 's/bedrock-server-\(.*\)\.zip/\1/')
+    else
+        echo "Failed to fetch Minecraft Bedrock server download link."
+        exit 1
+    fi
+}
+
+
+extractFiles() {
+    if [ ! -e "$FIRST_RUN_FLAG" ]; then
+        # First run: Extract all files to the destination folder
+        unzip -o "$TEMP_PATH/bedrock-server.zip" -d "$MAIN_PATH" > /dev/null 2>&1
+        touch "$FIRST_RUN_FLAG"  # Create the first run flag file
+        echo "First run: All files extracted to $MAIN_PATH."
+    else
+        # Subsequent runs: Extract only non-protected files to a temporary folder
+        unzip -o "$TEMP_PATH/bedrock-server.zip" -d "$TEMP_PATH/extracted" > /dev/null 2>&1
+        
+        if [ $? -ne 0 ]; then
+            echo "Failed to extract files."
+            exit 1
+        fi
+        
+        # Move only non-protected files from the temporary folder to the destination folder
+        rsync -a --ignore-existing "$TEMP_PATH/extracted/" "$MAIN_PATH/"
+        
+        # Clean up the temporary extracted files folder
+        rm -rf "$TEMP_PATH/extracted"
+        echo "Subsequent run: Non-protected files extracted and moved successfully."
+    fi
+}
+
+
+updateMinecraftServer() {
+if [[ "$LATEST_VER" == "$(tail -n 1 "$SERVER_DOWNLOAD_LOG")" ]]; then
+        echo "Server is already up to date. Skipping download."
+    else
+        # Download the latest version
+        echo "Downloading Minecraft Bedrock server version: $LATEST_VER from $LATEST_URL"
+        curl -o "$TEMP_PATH/bedrock-server.zip" "$LATEST_URL"
+        
+        # Update SERVER_DOWNLOAD_LOG
+        echo "$LATEST_VER" >> "$SERVER_DOWNLOAD_LOG"
+    fi
+    extractFiles
+}
+
+
+initialize_server() {
+cd "$MAIN_PATH" || exit
+getLatestVersion
+updateMinecraftServer
+}
 #### --- FUNCTIONS END --- ####
 
 
@@ -38,13 +94,13 @@ initialize_dirs() {
 # Main function
 main() {
     # Generate directory structure
-    initialize_dirs
+    initDirs
 
     # Perform server initialization
     initialize_server
 
     # Start server using Box64
-    box64 $BEDROCK_SERVER_PATH
+    #box64 $BEDROCK_SERVER_PATH
 
     # Additional post-server-startup tasks (if needed)
     # ...
@@ -57,8 +113,13 @@ main
 echo "MAIN_PATH: $MAIN_PATH"
 echo "DEBUG_PATH: $DEBUG_PATH"
 echo "TEMP_PATH: $TEMP_PATH"
+echo "LOG_PATH: $LOG_PATH"
+echo "SERVER_DOWNLOAD_LOG: $SERVER_DOWNLOAD_LOG"
+echo "LATEST_VER: $LATEST_VER"
+echo "LATEST_URL: $LATEST_URL"
 echo "BEDROCK_SERVER_PATH: $BEDROCK_SERVER_PATH"
 echo "SERVER_NAME: $SERVER_NAME"
 echo "MAX_PLAYERS: $MAX_PLAYERS"
 echo "SERVER_PORT: $SERVER_PORT"
+cat 
 #### --- DEBUGGING END --- ####
